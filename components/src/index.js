b9e64e02ecdec12747eb86cabc89ef197af15537
Sri Devan <sria@bgsu.edu>
2020-04-03 01:25:22 -0400

    Added a time prop called timeAgo

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Gana"/> 
-            <CommentDetail author="Gree"/> 
-            <CommentDetail author="Nagen"/>    
+            <CommentDetail author="Gana" timeAgo='Today at 4.45pm'/> 
+            <CommentDetail author="Gree" timeAgo='Yesterday at 3.00pm'/> 
+            <CommentDetail author="Nagen" timeAgo='Today at 2.00am'/>    
         </div>
     );
 }
